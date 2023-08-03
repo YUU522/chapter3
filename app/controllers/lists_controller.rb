c330e8d8aa44e757b3196a793830473dfa63551e
YUU522 <kuromu2750@gmail.com>
2023-08-03 07:01:08 +0000

    [Update]3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,10 +17,26 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])  
   end
 
+ def create
+    list = List.new(list_params)
+    list.save
+    # redirect_to '/top' を削除して、以下コードに変更
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)  end
+
   def edit
+     @list = List.find(params[:id])
   end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
+  end
+ 
   private
   # ストロングパラメータ
   def list_params
